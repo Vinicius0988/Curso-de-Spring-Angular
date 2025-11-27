21b88b44a22c3dffada43b8cc8da1f8749ac2dab
Vinicius_siqueira098 <viniv908@gmail.com>
2025-11-26 21:23:34 -0300

    inicios das aulas

--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'cursos' },
+  {
+    path: 'cursos',
+    loadComponent: () =>
+      import('./cursos/cursos').then(m => m.Cursos),
+  }
+];
---
+++ crud-angular/src/app/cursos/cursos.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos',
+  standalone: true,
+  imports: [],
+  templateUrl: './cursos.html',
+  styleUrl: './cursos.scss',
+})
+export class Cursos {
+
+}
