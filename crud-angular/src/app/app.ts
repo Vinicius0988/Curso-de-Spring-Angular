56cea3369c8e2b995f4606227153c3189b36f94f
Vinicius_siqueira098 <viniv908@gmail.com>
2025-11-29 19:46:25 -0300

    tabela feita

--- crud-angular/src/app/app.ts
+++ crud-angular/src/app/app.ts
@@ -1,12 +1,13 @@
+import { Cursos } from './cursos/cursos';
 import { Component, signal } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports:
-  [MatToolbarModule],
+  standalone: true,
+  imports: [MatToolbarModule,Cursos],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('crud-angular');
--- crud-angular/src/app/cursos/cursos.ts
+++ crud-angular/src/app/cursos/cursos.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Curso } from './modelos/Curso';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './cursos.html',
-  styleUrl: './cursos.scss',
+  styleUrls: ['./cursos.scss'],
 })
-export class Cursos {
+export class Cursos implements OnInit {
 
+  courses: Curso[] = [
+  { id: '1', name: 'Angular Básico', categoria: 'Frontend' },
+  { id: '2', name: 'Spring Boot', categoria: 'Backend' }
+];
+
+  displayedColumns: string[] = ['name', 'categoria'];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
